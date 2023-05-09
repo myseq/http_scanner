8b32094b7d5dac3782fb3813483c341d48d129a8
zd <myseq@users.noreply.github.com>
2023-05-09 22:25:26 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# =*- coding: utf-8 -*-
+
+import time
+from timeit import default_timer as timer
+from icecream import ic
+from colorama import init, Fore, Back, Style
+import argparse
+
+import asyncio
+import httpx
+
